d98b1c1891b25375e3e0b3f99ff205074c26cd02
aKqir24 <Akqir@protonmail.com>
2026-07-24 09:19:29 +0800

    upd: refine widgets and bar functionality

--- widgets/calendar/launch.sh
+++ widgets/calendar/launch.sh
@@ -2,5 +2,6 @@
 if eww active-windows 2>/dev/null | grep -q calendar; then
     eww close calendar
 else
+    eww update cal_day=$(date +%-d) cal_month_num=$(date +%-m) cal_year=$(date +%Y)
     eww open calendar
 fi
